1115dadf49ce25ebdc5a0fbf8ae6f17d26b86aba
PkaOnGit <pond.pakkapol@gmail.com>
2024-05-29 16:17:13 +0545

    Notification module

--- CSSmerge/Notification/models.py
+++ CSSmerge/Notification/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Ticket.models import Ticket
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Notification(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    title = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
--- CSSmerge/Ticket/serializers.py
+++ CSSmerge/Ticket/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Ticket, Category
 
-
 # Serializers define the API representation.
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
